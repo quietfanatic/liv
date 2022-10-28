864e1a38128b107fe3a554b85ccf2f73fcf5bbff
quietfanatic <lewis@wall.org>
2022-10-27 19:48:36 -0700

    glow: Fix texture program test (mipmapping problem)

--- src/base/glow/texture-program.cpp
+++ src/base/glow/texture-program.cpp
@@ -14,7 +14,11 @@ struct TextureProgram : Program {
     void Program_after_link () override {
         u_screen_rect = glGetUniformLocation(id, "u_screen_rect");
         u_tex_rect = glGetUniformLocation(id, "u_tex_rect");
-        glUniform1i(glGetUniformLocation(id, "u_2d_tex"), 0);
+        int u_tex = glGetUniformLocation(id, "u_tex");
+        glUniform1i(u_tex, 0);
+        AA(u_screen_rect != -1);
+        AA(u_tex_rect != -1);
+        AA(u_tex != -1);
     }
 };
 
@@ -39,6 +43,8 @@ HACCABLE(glow::TextureProgram,
 
 #ifndef TAP_DISABLE_TESTS
 #include <vector>
+#include <SDL2/SDL_timer.h>
+#include <SDL2/SDL_video.h>
 #include "../hacc/serialize.h"
 #include "../tap/tap.h"
 #include "../wind/window.h"
@@ -61,7 +67,7 @@ static tap::TestSet tests ("base/glow/texture-program", []{
     }, "Can load texture");
 
     RGBA8 bg = uint32(0x331100ee);
-    RGBA8 fg = uint32(0x2674dbff);
+    RGBA8 fg = uint32(0x2674dbf0);
 
     int width; glGetTexLevelParameteriv(tex->target, 0, GL_TEXTURE_WIDTH, &width);
     is(width, 7, "Created texture has correct width");
@@ -70,7 +76,7 @@ static tap::TestSet tests ("base/glow/texture-program", []{
 
     Image tex_image (tex->source.size());
     glGetTexImage(tex->target, 0, GL_RGBA, GL_UNSIGNED_BYTE, tex_image.pixels);
-    is(tex_image[{1, 1}], fg, "Created texture has correct content");
+    is(tex_image[{4, 3}], fg, "Created texture has correct content");
 
     glClearColor(bg.r/255.f, bg.g/255.f, bg.b/255.f, bg.a/255.f);
     glClear(GL_COLOR_BUFFER_BIT);
@@ -111,6 +117,8 @@ static tap::TestSet tests ("base/glow/texture-program", []{
 //        expected.save(hacc::resource_filename("/base/glow/test/texture-fail-expected"));
 //        got.save(hacc::resource_filename("/base/glow/test/texture-fail-got"));
     }
+//    SDL_GL_SwapWindow(window.sdl_window);
+//    SDL_Delay(5000);
 
     done_testing();
 });
