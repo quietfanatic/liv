891a3532070344eb8ed724c153369fa89fc22065
quietfanatic <lewis@wall.org>
2022-12-19 14:24:38 -0800

    ayu: Optimize print by using char*s instead of String

--- src/base/ayu/src/print.cpp
+++ src/base/ayu/src/print.cpp
@@ -1,5 +1,6 @@
 #include "../print.h"
 
+#include <cstring>
 #include <charconv>
 
 #include "../compat.h"
@@ -13,159 +14,206 @@ namespace ayu {
 namespace in {
 
 struct Printer {
-    String out;
     PrintOptions opts;
+    char* start;
+    char* end;
 
-    Printer (PrintOptions f) : opts(f) { }
+    Printer (PrintOptions f) :
+        opts(f),
+        start((char*)malloc(256)),
+        end(start + 256)
+    { }
 
-    void print_quoted (Str s, bool expand = false) {
-        out += '"';
-        for (auto p = s.begin(); p != s.end(); p++)
-        switch (*p) {
-            case '"': out += "\\\""sv; break;
-            case '\\': out += "\\\\"sv; break;
-            case '\b': out += "\\b"sv; break;
-            case '\f': out += "\\f"sv; break;
-            case '\n':
-                if (expand) out += *p;
-                else out += "\\n"sv;
-                break;
-            case '\r': out += "\\r"sv; break;
-            case '\t':
-                if (expand) out += *p;
-                else out += "\\t"sv;
-                break;
-            default: out += *p; break;
+    ~Printer () { free(start); }
+
+    [[gnu::noinline]]
+    [[nodiscard]]
+    char* extend (char* p, usize more) {
+        usize new_size = end - start;
+        while (new_size < p - start + more) {
+            new_size *= 2;
         }
-        out += '"';
+        char* old_start = start;
+        start = (char*)realloc(start, new_size);
+        end = start + new_size;
+        return p - old_start + start;
+    }
+    [[gnu::noinline]]
+    [[nodiscard]]
+    char* extend_1 (char* p) {
+        usize new_size = (end - start) * 2;
+        char* old_start = start;
+        start = (char*)realloc(start, new_size);
+        end = start + new_size;
+        return p - old_start + start;
     }
 
-    void print_string (Str s, bool expand = false) {
-        if (opts & JSON) return print_quoted(s, false);
-        if (s == ""sv) {
-            out += "\"\""sv; return;
+    [[nodiscard]]
+    char* pchar (char* p, char c) {
+        if (p == end) [[unlikely]] p = extend_1(p);
+        *p = c;
+        return p+1;
+    }
+    [[nodiscard]]
+    char* pstr (char* p, Str s) {
+        if (p + s.size() > end) [[unlikely]] p = extend(p, s.size());
+        std::memcpy(p, s.data(), s.size());
+        return p + s.size();
+    }
+
+    [[nodiscard]]
+    char* print_uint64 (char* p, uint64 v, bool hex) {
+        if (v == 0) {
+            return pchar(p, '0');
         }
-        else if (s == "null"sv) {
-            out += "\"null\""sv; return;
+        if (end - p < 20) [[unlikely]] p = extend(p, 20);
+        auto [ptr, ec] = std::to_chars(
+            p, p+20, v, hex ? 16 : 10
+        );
+        if (ec != std::errc()) AYU_INTERNAL_UGUU();
+        return ptr;
+    }
+
+    [[nodiscard]]
+    char* print_int64 (char* p, int64 v, bool hex) {
+        if (v == 0) {
+            return pchar(p, '0');
         }
-        else if (s == "true"sv) {
-            out += "\"true\""sv; return;
+        if (v < 0) {
+            p = pchar(p, '-');
         }
-        else if (s == "false"sv) {
-            out += "\"false\""sv; return;
+        if (hex) {
+            p = pstr(p, "0x"sv);
+        }
+        return print_uint64(p, v < 0 ? -v : v, hex);
+    }
+
+    [[nodiscard]]
+    char* print_double (char* p, double v, bool hex) {
+        if (hex) {
+            if (v < 0) {
+                p = pchar(p, '-');
+                v = -v;
+            }
+            p = pstr(p, "0x"sv);
+        }
+        if (end - p < 24) [[unlikely]] p = extend(p, 24);
+        auto [ptr, ec] = std::to_chars(
+            p, p+24, v, hex
+                ? std::chars_format::hex
+                : std::chars_format::general
+        );
+        if (ec != std::errc()) AYU_INTERNAL_UGUU();
+        return ptr;
+    }
+
+    [[nodiscard]]
+    char* print_quoted (char* p, Str s, bool expand) {
+        p = pchar(p, '"');
+        for (auto c : s)
+        switch (c) {
+            case '"': p = pstr(p, "\\\""sv); break;
+            case '\\': p = pstr(p, "\\\\"sv); break;
+            case '\b': p = pstr(p, "\\b"sv); break;
+            case '\f': p = pstr(p, "\\f"sv); break;
+            case '\n':
+                if (expand) p = pchar(p, c);
+                else p = pstr(p, "\\n"sv);
+                break;
+            case '\r': p = pstr(p, "\\r"sv); break;
+            case '\t':
+                if (expand) p = pchar(p, c);
+                else p = pstr(p, "\\t"sv);
+                break;
+            default: p = pchar(p, c); break;
+        }
+        return pchar(p, '"');
+    }
+
+    [[nodiscard]]
+    char* print_string (char* p, Str s, bool expand) {
+        if (opts & JSON) {
+            return print_quoted(p, s, false);
+        }
+        if (s == ""sv || s == "null"sv || s == "true"sv || s == "false"sv) {
+            return pchar(pstr(pchar(p, '"'), s), '"');
         }
         switch (s[0]) {
             case ANY_LETTER:
             case '_': break;
-            default: return print_quoted(s, expand);
+            default: return print_quoted(p, s, expand);
         }
 
-        for (auto p = s.begin(); p != s.end(); p++)
-        switch (p[0]) {
+        for (auto sp = s.begin(); sp != s.end(); sp++)
+        switch (sp[0]) {
             case ':': {
-                 // Don't need to check bounds because String is NUL-terminated
-                if (p[1] == ':' || p[1] == '/') {
-                    p++;
+                 // Don't need to check bounds because s should always be
+                 // NUL-terminated
+                if (sp[1] == ':' || sp[1] == '/') {
+                    sp++;
                     continue;
                 }
-                else return print_quoted(s, expand);
+                else return print_quoted(p, s, expand);
             }
             case ANY_LETTER: case ANY_DECIMAL_DIGIT:
             case '-': case '.': case '/': case '_': continue;
-            default: return print_quoted(s, expand);
+            default: return print_quoted(p, s, expand);
         }
          // No need to quote
-        out += s;
+        return pstr(p, s);
     }
 
-    void print_newline (uint n) {
-        out += '\n';
-        for (; n; n--) out += "    "sv;
+    [[nodiscard]]
+    char* print_newline (char* p, uint n) {
+        p = pchar(p, '\n');
+        for (; n; n--) p = pstr(p, "    "sv);
+        return p;
     }
 
-    void print_tree (const Tree& t, uint ind) {
+    [[nodiscard]]
+    char* print_subtree (char* p, const Tree& t, uint ind) {
         uint16 flags = t.data->flags;
         switch (t.data->rep) {
-            case Rep::NULLREP:
-                out += "null"sv;
-                return;
-            case Rep::BOOL:
-                out += (t.data->as_known<bool>()
-                    ? "true"sv : "false"sv
-                );
-                return;
+            case Rep::NULLREP: return pstr(p, "null"sv);
+            case Rep::BOOL: {
+                Str s = t.data->as_known<bool>() ? "true"sv : "false"sv;
+                return pstr(p, s);
+            }
             case Rep::INT64: {
-                int64 v = t.data->as_known<int64>();
-                if (v == 0) {
-                    out += '0';
-                }
-                else {
-                    bool hex = !(opts & JSON) && flags & PREFER_HEX;
-                    if (v < 0) {
-                        out += '-';
-                    }
-                    if (hex) {
-                        out += "0x";
-                    }
-                    uint64 mag = v < 0 ? -v : v;
-                    char buf [32];
-                    auto [ptr, ec] = std::to_chars(
-                        buf, buf+32, mag, hex ? 16 : 10
-                    );
-                    if (ec != std::errc()) AYU_INTERNAL_UGUU();
-                    out += Str(buf, ptr - buf);
-                }
-                return;
+                bool hex = !(opts & JSON) && flags & PREFER_HEX;
+                return print_int64(p, t.data->as_known<int64>(), hex);
             }
             case Rep::DOUBLE: {
                 double v = t.data->as_known<double>();
                 if (v != v) {
-                    if (opts & JSON) out += "null"sv;
-                    else out += "+nan"sv;
+                    return pstr(p, opts & JSON ? "null"sv : "+nan"sv);
                 }
                 else if (v == 1.0/0.0) {
-                    if (opts & JSON) out += "1e999"sv;
-                    else out += "+inf"sv;
+                    return pstr(p, opts & JSON ? "1e999"sv : "+inf"sv);
                 }
                 else if (v == -1.0/0.0) {
-                    if (opts & JSON) out += "-1e999"sv;
-                    else out += "-inf"sv;
+                    return pstr(p, opts & JSON ? "-1e999"sv : "-inf"sv);
                 }
                 else if (v == 0) {
                     if (1.0/v == -inf) {
-                        out += "-0"sv;
+                        p = pchar(p, '-');
                     }
-                    else {
-                        out += "0"sv;
-                    }
+                    return pchar(p, '0');
                 }
                 else {
                     bool hex = !(opts & JSON) && flags & PREFER_HEX;
-                    if (hex) {
-                        if (v < 0) {
-                            out += '-';
-                            v = -v;
-                        }
-                        out += "0x"sv;
-                    }
-                    char buf [32]; // Should be enough?
-                    auto [ptr, ec] = std::to_chars(
-                        buf, buf+32, v, hex
-                            ? std::chars_format::hex
-                            : std::chars_format::general
-                    );
-                    if (ec != std::errc()) AYU_INTERNAL_UGUU();
-                    out += Str(buf, ptr - buf);
+                    return print_double(p, v, hex);
                 }
-                return;
             }
             case Rep::STRING:
                 return print_string(
-                    t.data->as_known<String>(), flags & PREFER_EXPANDED
+                    p, t.data->as_known<String>(), flags & PREFER_EXPANDED
                 );
             case Rep::ARRAY: {
                 const Array& a = t.data->as_known<Array>();
-                if (a.empty()) { out += "[]"sv; return; }
+                if (a.empty()) {
+                    return pstr(p, "[]"sv);
+                }
 
                  // Print "small" arrays compactly.
                  // TODO: Revise this?
@@ -177,33 +225,34 @@ struct Printer {
                 bool show_indices = expand
                                  && a.size() > 4
                                  && !(opts & JSON);
-                out += '[';
+                p = pchar(p, '[');
                 for (auto& elem : a) {
                     if (&elem == &a.front()) {
-                        if (expand) print_newline(ind + 1);
+                        if (expand) p = print_newline(p, ind + 1);
                     }
                     else {
                         if (expand) {
-                            if (opts & JSON) out += ',';
-                            print_newline(ind + 1);
+                            if (opts & JSON) p = pchar(p, ',');
+                            p = print_newline(p, ind + 1);
                         }
                         else {
-                            if (opts & JSON) out += ',';
-                            else out += ' ';
+                            p = pchar(p, opts & JSON ? ',' : ' ');
                         }
                     }
-                    print_tree(elem, ind + expand);
+                    p = print_subtree(p, elem, ind + expand);
                     if (show_indices) {
-                        out = cat(out, "  // "sv, (&elem - &a.front()));
+                        p = pstr(p, "  // "sv);
+                        p = print_int64(p, &elem - &a.front(), false);
                     }
                 }
-                if (expand) print_newline(ind);
-                out += ']';
-                return;
+                if (expand) p = print_newline(p, ind);
+                return pchar(p, ']');
             }
             case Rep::OBJECT: {
                 const Object& o = t.data->as_known<Object>();
-                if (o.empty()) { out += "{}"sv; return; }
+                if (o.empty()) {
+                    return pstr(p, "{}"sv);
+                }
 
                  // TODO: Decide what to do if both PREFER flags are set
                 bool expand = !(opts & PRETTY) ? false
@@ -211,29 +260,29 @@ struct Printer {
                             : flags & PREFER_COMPACT ? false
                             : o.size() > 1;
 
-                out += '{';
+                p = pchar(p, '{');
                 for (auto& attr : o) {
                     if (&attr == &o.front()) {
-                        if (expand) print_newline(ind + 1);
+                        if (expand) {
+                            p = print_newline(p, ind + 1);
+                        }
                     }
                     else {
                         if (expand) {
-                            if (opts & JSON) out += ',';
-                            print_newline(ind + 1);
+                            if (opts & JSON) p = pchar(p, ',');
+                            p = print_newline(p, ind + 1);
                         }
                         else {
-                            if (opts & JSON) out += ',';
-                            else out += ' ';
+                            p = pchar(p, opts & JSON ? ',' : ' ');
                         }
                     }
-                    print_string(attr.first);
-                    out += ':';
-                    if (expand) out += ' ';
-                    print_tree(attr.second, ind + expand);
+                    p = print_string(p, attr.first, false);
+                    p = pchar(p, ':');
+                    if (expand) p = pchar(p, ' ');
+                    p = print_subtree(p, attr.second, ind + expand);
                 }
-                if (expand) print_newline(ind);
-                out += '}';
-                return;
+                if (expand) p = print_newline(p, ind);
+                return pchar(p, '}');
             }
             case Rep::ERROR: {
                 try {
@@ -241,18 +290,26 @@ struct Printer {
                         t.data->as_known<std::exception_ptr>()
                     );
                 }
-                catch (const Error& e) {
+                catch (const std::exception& e) {
+                    Str name;
                     try {
-                        out += cat("?("sv, Type(typeid(e)).name(), ')');
+                        name = Type(typeid(e)).name();
                     }
                     catch (const UnknownType&) {
-                        out += cat("?("sv, typeid(e).name(), ')');
+                        name = typeid(e).name();
                     }
+                    return pchar(pstr(pstr(p, "?("sv), name), ')');
                 }
-                break;
+                AYU_INTERNAL_UGUU();
             }
             default: AYU_INTERNAL_UGUU();
         }
+    }
+    [[nodiscard]]
+    char* print_tree (char* p, const Tree& t) {
+        p = print_subtree(p, t, 0);
+        if (opts & PRETTY) p = pchar(p, '\n');
+        return p;
     }
 };
 
@@ -270,10 +327,9 @@ static void validate_print_options (PrintOptions opts) {
 String tree_to_string (const Tree& t, PrintOptions opts) {
     validate_print_options(opts);
     if (!(opts & PRETTY)) opts |= COMPACT;
-    Printer p (opts);
-    p.print_tree(t, 0);
-    if (opts & PRETTY) p.out += '\n';
-    return std::move(p.out);
+    Printer printer (opts);
+    char* p = printer.print_tree(printer.start, t);
+    return String(printer.start, p - printer.start);
 }
 
  // Forget C++ IO and its crummy diagnostics
@@ -288,10 +344,12 @@ void string_to_file (Str content, Str filename) {
     }
 }
 
-void tree_to_file (const Tree& tree, Str filename, PrintOptions opts) {
+void tree_to_file (const Tree& t, Str filename, PrintOptions opts) {
     validate_print_options(opts);
     if (!(opts & COMPACT)) opts |= PRETTY;
-    return string_to_file(tree_to_string(tree, opts), filename);
+    Printer printer (opts);
+    char* p = printer.print_tree(printer.start, t);
+    string_to_file(Str(printer.start, p - printer.start), filename);
 }
 
 } using namespace ayu;
