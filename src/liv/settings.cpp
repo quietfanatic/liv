7655da4b5bf9bb252541b9d30029a9c0691be6a2
quietfanatic <lewis@wall.org>
2023-12-29 13:00:23 -0800

    Update dirt and use new collapse_optional flag

--- src/liv/settings.cpp
+++ src/liv/settings.cpp
@@ -105,48 +105,48 @@ AYU_DESCRIBE(liv::Mapping,
 
 AYU_DESCRIBE(liv::LayoutSettings,
     attrs(
-        attr("spread_count", &LayoutSettings::spread_count, optional),
-        attr("spread_direction", &LayoutSettings::spread_direction, optional),
-        attr("auto_zoom_mode", &LayoutSettings::auto_zoom_mode, optional),
-        attr("reset_zoom_on_page_turn", &LayoutSettings::reset_zoom_on_page_turn, optional),
-        attr("max_zoom", &LayoutSettings::max_zoom, optional),
-        attr("min_zoomed_size", &LayoutSettings::min_zoomed_size, optional),
-        attr("small_align", &LayoutSettings::small_align, optional),
-        attr("large_align", &LayoutSettings::large_align, optional)
+        attr("spread_count", &LayoutSettings::spread_count, collapse_optional),
+        attr("spread_direction", &LayoutSettings::spread_direction, collapse_optional),
+        attr("auto_zoom_mode", &LayoutSettings::auto_zoom_mode, collapse_optional),
+        attr("reset_zoom_on_page_turn", &LayoutSettings::reset_zoom_on_page_turn, collapse_optional),
+        attr("max_zoom", &LayoutSettings::max_zoom, collapse_optional),
+        attr("min_zoomed_size", &LayoutSettings::min_zoomed_size, collapse_optional),
+        attr("small_align", &LayoutSettings::small_align, collapse_optional),
+        attr("large_align", &LayoutSettings::large_align, collapse_optional)
     )
 )
 AYU_DESCRIBE(liv::PageSettings,
     attrs(
-        attr("interpolation_mode", &PageSettings::interpolation_mode, optional)
+        attr("interpolation_mode", &PageSettings::interpolation_mode, collapse_optional)
     )
 )
 
 AYU_DESCRIBE(liv::WindowSettings,
     attrs(
-        attr("size", &WindowSettings::size, optional),
-        attr("fullscreen", &WindowSettings::fullscreen, optional),
-        attr("window_background", &WindowSettings::window_background, optional)
+        attr("size", &WindowSettings::size, collapse_optional),
+        attr("fullscreen", &WindowSettings::fullscreen, collapse_optional),
+        attr("window_background", &WindowSettings::window_background, collapse_optional)
     )
 )
 
 AYU_DESCRIBE(liv::FilesSettings,
     attrs(
-        attr("supported_extensions", &FilesSettings::supported_extensions, optional)
+        attr("supported_extensions", &FilesSettings::supported_extensions, collapse_optional)
     )
 )
 
 AYU_DESCRIBE(liv::ControlSettings,
     attrs(
-        attr("drag_speed", &ControlSettings::drag_speed, optional)
+        attr("drag_speed", &ControlSettings::drag_speed, collapse_optional)
     )
 )
 
 AYU_DESCRIBE(liv::MemorySettings,
     attrs(
-        attr("preload_ahead", &MemorySettings::preload_ahead, optional),
-        attr("preload_behind", &MemorySettings::preload_behind, optional),
-        attr("page_cache_mb", &MemorySettings::page_cache_mb, optional),
-        attr("trim_when_minimized", &MemorySettings::trim_when_minimized, optional)
+        attr("preload_ahead", &MemorySettings::preload_ahead, collapse_optional),
+        attr("preload_behind", &MemorySettings::preload_behind, collapse_optional),
+        attr("page_cache_mb", &MemorySettings::page_cache_mb, collapse_optional),
+        attr("trim_when_minimized", &MemorySettings::trim_when_minimized, collapse_optional)
     )
 )
 
@@ -158,6 +158,6 @@ AYU_DESCRIBE(liv::Settings,
         attr("control", base<ControlSettings>(), optional),
         attr("files", base<FilesSettings>(), optional),
         attr("memory", base<MemorySettings>(), optional),
-        attr("mappings", &Settings::mappings)
+        attr("mappings", &Settings::mappings, optional)
     )
 )
