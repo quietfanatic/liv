b41e442789bae58ed8555b43bb56a61661f5f4b8
quietfanatic <lewis@wall.org>
2023-12-30 20:32:56 -0800

    Make sorting by file attributes measurably faster

--- src/liv/sort.cpp
+++ src/liv/sort.cpp
@@ -11,42 +11,95 @@
 namespace liv {
 
 void do_sort (IRI* begin, IRI* end, SortMethod method) {
-    if (method.criterion == SortCriterion::Unsorted) {
-        return;
-    }
-    else if (method.criterion == SortCriterion::Shuffle) {
-        static std::random_device rd;
-        static std::mt19937 g (rd());
-        std::shuffle(begin, end, g);
-        return;
-    }
-    std::stable_sort(begin, end, [method](const IRI& aa, const IRI& bb){
-        const IRI& a = !!(method.flags & SortFlags::Reverse)
-            ? bb : aa;
-        const IRI& b = !!(method.flags & SortFlags::Reverse)
-            ? aa : bb;
-        switch (method.criterion) {
-            case SortCriterion::Natural: {
-                return uni::natural_lessthan(a.path(), b.path());
+    switch (method.criterion) {
+        case SortCriterion::Natural: {
+            if (!!(method.flags & SortFlags::Reverse)) {
+                std::stable_sort(begin, end, [](const IRI& a, const IRI& b){
+                    return uni::natural_lessthan(b.path(), a.path());
+                });
             }
-            case SortCriterion::Unicode: {
-                return a.path() < b.path();
+            else {
+                std::stable_sort(begin, end, [](const IRI& a, const IRI& b){
+                    return uni::natural_lessthan(a.path(), b.path());
+                });
             }
-            case SortCriterion::LastModified: {
-                auto atime = fs::last_write_time(iri::to_fs_path(a));
-                auto btime = fs::last_write_time(iri::to_fs_path(b));
-                if (atime != btime) return atime < btime;
-                else return uni::natural_lessthan(a.path(), b.path());
-            }
-            case SortCriterion::FileSize: {
-                auto asize = fs::file_size(iri::to_fs_path(a));
-                auto bsize = fs::file_size(iri::to_fs_path(b));
-                if (asize != bsize) return asize < bsize;
-                else return uni::natural_lessthan(a.path(), b.path());
-            }
-            default: never();
+            break;
         }
-    });
+        case SortCriterion::Unicode: {
+            if (!!(method.flags & SortFlags::Reverse)) {
+                std::stable_sort(begin, end, [](const IRI& a, const IRI& b){
+                    return b.path() < a.path();
+                });
+            }
+            else {
+                std::stable_sort(begin, end, [](const IRI& a, const IRI& b){
+                    return a.path() < b.path();
+                });
+            }
+            break;
+        }
+        case SortCriterion::LastModified: {
+             // Getting file mod times is slow, especially with the
+             // std::filesystem library, so cache the results
+            using Time = decltype(fs::last_write_time(fs::path()));
+            auto proj = UniqueArray<std::pair<Time, IRI>>(
+                end - begin, [begin](usize i){
+                    return std::pair<Time, IRI>{
+                        fs::last_write_time(iri::to_fs_path(begin[i])),
+                        move(begin[i])
+                    };
+                }
+            );
+             // Do the sort
+            if (!!(method.flags & SortFlags::Reverse)) {
+                std::stable_sort(proj.begin(), proj.end(), [](const auto& a, const auto& b){
+                    return b.first < a.first;
+                });
+            }
+            else {
+                std::stable_sort(proj.begin(), proj.end(), [](const auto& a, const auto& b){
+                    return a.first < b.first;
+                });
+            }
+             // Undo the projection
+            proj.consume([begin, b{proj.begin()}](const auto& p){
+                new (&begin[&p - b]) IRI(move(p.second));
+            });
+            break;
+        }
+        case SortCriterion::FileSize: {
+            auto proj = UniqueArray<std::pair<usize, IRI>>(
+                end - begin, [begin](usize i){
+                    return std::pair<usize, IRI>{
+                        fs::file_size(iri::to_fs_path(begin[i])),
+                        move(begin[i])
+                    };
+                }
+            );
+            if (!!(method.flags & SortFlags::Reverse)) {
+                std::stable_sort(proj.begin(), proj.end(), [](const auto& a, const auto& b){
+                    return b.first < a.first;
+                });
+            }
+            else {
+                std::stable_sort(proj.begin(), proj.end(), [](const auto& a, const auto& b){
+                    return a.first < b.first;
+                });
+            }
+            proj.consume([begin, b{proj.begin()}](const auto& p){
+                new (&begin[&p - b]) IRI(move(p.second));
+            });
+            break;
+        }
+        case SortCriterion::Shuffle: {
+            static std::random_device rd;
+            static std::mt19937 g (rd());
+            std::shuffle(begin, end, g);
+            break;
+        }
+        case SortCriterion::Unsorted: break;
+        default: never();
+    }
 }
 
 struct SortMethodToken : SortMethod { };
